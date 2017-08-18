60a8e97403b3d9d9e7975fa91851736acbf39394
bo du <bodu93@outlook.com>
2017-08-18 16:08:02 +0800

    add triple_segmentation algorithms

--- sort/quicksort/quicksort.cc
+++ sort/quicksort/quicksort.cc
@@ -1,5 +1,6 @@
 #include "partition.h"
-#include <iostream>
+#include "../include/swp.h"
+#include "../include/utility.h"
 #include <vector>
 
 void quicksort(int *A, size_t p, size_t q) {
@@ -11,6 +12,32 @@ void quicksort(int *A, size_t p, size_t q) {
 	}
 }
 
+// A[lo, hi)
+void triple_segmentation(int *A, size_t lo, size_t hi) {
+	if (lo >= hi) return;
+	
+	size_t lt = lo;
+	size_t i = lo + 1;
+	size_t gt = hi;
+	int v = A[lo];
+	while (i != gt) {
+		if (A[i] < v) {
+			swap_(A + i, A + lt);
+			++i;
+			++lt;
+		} else if (A[i] > v) {
+			--gt;
+			swap_(A + i, A + gt);
+		} else {
+			++i;
+		}
+	}
+		
+	triple_segmentation(A, lo, lt);
+	triple_segmentation(A, gt, hi);
+}
+
 void qsort_(int *A, size_t n) {
-	quicksort(A, 0, n);
+	//quicksort(A, 0, n);
+	triple_segmentation(A, 0, n);
 }
