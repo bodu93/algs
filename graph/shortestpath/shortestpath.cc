68410e9c2de2412999a05c1c870de60aa775e4e0
bo du <bodu93@outlook.com>
2017-08-28 22:52:47 +0800

    add shortestpath algo(TODO: compile and test)

---
+++ graph/shortestpath/shortestpath.cc
@@ -0,0 +1,73 @@
+#include "shortestpath.h"
+
+#include "utility/minpq.h"
+
+ShortestPath::ShortestPath(const graph &g)
+	:g_{g}
+	,vertexes_(g_.V())
+{ }
+
+bool ShortestPath::bellman_ford(size_t s) {
+	init(s);
+
+	for (size_t i = 1; i != g_.V() - 1; ++i) {
+		for (size_t u = 0; u != g_.V(); ++u) {
+			for (size_t v : g_.adj(u)) {
+				relax(u, v);
+			}
+		}
+	}
+
+	for (size_t u = 0; u != g_.V(); ++u) {
+		for (size_t v : g_.adj(u)) {
+			if (vertexes_[v].d > vertexes_[u].d + g.weight(u, v)) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+static bool vertex_equals(const vertex &lhs, const vertex &rhs) {
+	return lhs.rank == rhs.rank;
+}
+
+void ShortestPath::dijkstra(size_t s) {
+	init(s);
+
+	MinPQ<vertex> Q;
+	for (const auto &v : vertexes_)
+		Q.insert(v);
+
+	while (!Q.empty()) {
+		vertex unode = Q.delMin();
+		size_t u = unode.rank;
+		
+		for (size_t v : g_.adj(u)) {
+			relax(u, v);
+
+			if (vertexes_[v].d > vertexes_[u].d + g_.weight(u, v)) {
+				vertex findv;
+				findv.rank = v;
+				size_t idx = Q.findKey(findv, vertex_equals);
+				if (idx != static_cast<size_t>(-1)) {
+					Q.decreaseKey(idx, vertexes_[v].d);
+				}
+			}
+		}
+}
+
+void ShortestPath::init(size_t s) {
+	for (size_t i = 0; i != vertexes_.size(); ++i)
+		vertexes_[i].rank = i;
+
+	vertexes_[s].d = 0;
+}
+
+void ShortestPath::relax(size_t u, size_t v) {
+	if (vertexes_[v].d > vertexes_[u].d + g_.weight(u, v)) {
+		vertexes_[v].d = vertexes_[u].d + g_.weight(u, v);
+		vertexes_[v].parent = u;
+	}
+}
---
+++ graph/shortestpath/shortestpath.h
@@ -0,0 +1,36 @@
+#ifndef SHORTESTPATH_H
+#define SHORTESTPATH_H
+
+#include "../mst/graph.h"
+#include <limits>
+#include <vector>
+
+struct vertex {
+	size_t rank{-1};
+	size_t parent{-1};
+	double d{std::numeric_limits<double>::max()};
+
+	bool operator<(const vertex &other) const {
+		return d < other.d;
+	}
+
+	bool operator==(const vertex &other) const {
+		return d == other.d;
+	}
+};
+
+class ShortestPath {
+public:
+	explicit ShortestPath(const graph &g);
+
+	bool bellman_ford(size_t s);
+	void dijkstra(size_t s);
+private:
+	void init(size_t s);
+	void relax(size_t u, size_t v);
+
+	graph g_;
+	std::vector<vertex> vertexes_;
+};
+
+#endif // SHORTESTPATH_H
